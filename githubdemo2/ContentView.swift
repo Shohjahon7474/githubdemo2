b01c34f0d388dc2a541a6d35be680f28429e20e3
shokh <1mustafoman@gmail.com>
2022-03-19 18:40:30 +0500

    Initial Commit

---
+++ githubdemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubdemo2
+//
+//  Created by Shohjahon Numonovich on 3/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to SwiftUI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubdemo2/githubdemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemo2App.swift
+//  githubdemo2
+//
+//  Created by Shohjahon Numonovich on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct githubdemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
